0fbdf501c6f0421e855b805aff76ff6dd2fef231
Iulian Gulea <iulian.gulea@gmail.com>
2018-08-22 22:14:11 +0300

    Adjusted actions of job offer component based on its current status and user type.

--- frontend/src/components/JobOffer/index.js
+++ frontend/src/components/JobOffer/index.js
@@ -11,9 +11,9 @@ import * as contractsActionCreators from "../../actions/contracts";
 import {push} from "connected-react-router";
 import connect from "react-redux/es/connect/connect";
 import ipfs from "../../ipfs";
-import Fade from "@material-ui/core/Fade";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import LoadingSpinner from "../LoadingSpinner";
+import Tooltip from "@material-ui/core/Tooltip";
+import Snackbar from "@material-ui/core/Snackbar";
 
 const domainMapping = {
   0: 'IT',
@@ -66,7 +66,8 @@ function mapDispatchToProps(dispatch) {
       ...companiesActionCreators,
       ...contractsActionCreators,
       createJobOffer: (address) => push(`/company/${address}/jobOffer/create`),
-      manageJobOffer: (address, hash) => push(`/company/${address}/jobOffer/${hash}`)
+      manageJobOffer: (address, hash) => push(`/company/${address}/jobOffer/${hash}`),
+      goToCompanyPage: (address) => push(`/company/${address}`),
     }, dispatch);
 }
 
@@ -77,12 +78,14 @@ class JobOffer extends React.Component {
     ipfsHash: null,
     loadingTransaction: false,
     loadingIPFS: false,
-    eventName: ''
+    eventName: '',
+    open: false
   };
 
   constructor(props) {
     super(props);
     this.applyToJobOffer = this.applyToJobOffer.bind(this);
+    props.jobOffer.rewardInEther = window.web3.fromWei(props.jobOffer.rewardInWei);
   }
 
   componentDidMount = async () => {
@@ -95,12 +98,12 @@ class JobOffer extends React.Component {
       this.props.receiveJobOfferIPFSDetails(this.props.company.address, this.props.jobOffer.hash, jobDescription);
     });
 
-    this.props.companyContract.getApplicantsOfJobOffer(this.props.jobOffer.hash, (err, result) => {
-      if (err) return;
+    if (this.props.jobOffer.isPublished && this.props.jobOffer.isOpen) {
+      this.props.companyContract.getApplicantsOfJobOffer(this.props.jobOffer.hash, {from: this.props.userAddress, gas: 100000}, (err, result) => {
 
-      this.props.applicantsListReceivedAction(this.props.company.address, this.props.jobOffer.hash, result)
-    });
-
+        this.props.applicantsListReceivedAction(this.props.company.address, this.props.jobOffer.hash, result)
+      });
+    }
   };
 
   goToJobOfferPage(address, hash) {
@@ -128,7 +131,6 @@ class JobOffer extends React.Component {
     if (this.state.buffer) {
       this.setState({loadingIPFS: true, eventName: 'JobOfferPublished'});
       await ipfs.add(this.state.buffer, (err, ipfsHash) => {
-        console.log(err, ipfsHash);
 
         // Set state by setting ipfsHash to ipfsHash[0].hash
         self.setState({
@@ -164,23 +166,46 @@ class JobOffer extends React.Component {
     }
   };
 
+  closeJobOffer = async (hash) => {
+    this.setState({loadingTransaction: true, eventName: 'JobOfferClosed'});
+    this.props.companyContract.closeJobOffer(hash, false, {from: this.props.userAddress, gas: 500000}, (err, result) => console.log(err, result));
+
+    // Watch for the CompanyCreated event on the blockchain.
+    const jobOfferCloseEvent = this.props.companyContract.JobOfferClosed(null, {fromBlock: this.props.blockNr, toBlock: 'latest'});
+    jobOfferCloseEvent.watch((error, event) => {
+      if (error) console.log('ERROR!!!', error);
+      console.log('ERROR, EVENT', error, event);
+
+      if (event.args._companyName === this.props.company.name && event.args._jobTitle === this.props.jobOffer.title) {
+        this.setState({loadingTransaction: false});
+        this.props.closeJobOfferAction(this.props.company.address, this.props.jobOffer.hash);
+        this.props.goToCompanyPage(this.props.company.address);
+      }
+    });
+  };
+
   captureFile = (event) => {
-      event.stopPropagation();
-      event.preventDefault();
-      const file = event.target.files[0];
-      let reader = new window.FileReader();
-      reader.readAsArrayBuffer(file);
-      reader.onloadend = () => this.convertToBuffer(reader);
-      console.log('capture file final');
-    };
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    let reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => this.convertToBuffer(reader);
+  };
 
   convertToBuffer = async(reader) => {
-      //file is converted to a buffer to prepare for uploading to IPFS
-      const buffer = await Buffer.from(reader.result);
-      //set this buffer -using es6 syntax
-      this.setState({buffer});
-      console.log('convert to buffer final');
-      console.log(this.state);
+    //file is converted to a buffer to prepare for uploading to IPFS
+    const buffer = await Buffer.from(reader.result);
+    //set this buffer -using es6 syntax
+    this.setState({buffer});
+  };
+
+  withdraw = async (amount) => {
+    this.props.companyContract.withdrawReward(this.props.jobOffer.hash, amount, {from: this.props.userAddress, gas: 500000}, (err, result) => console.log(err, result));
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
   };
 
   render() {
@@ -202,9 +227,15 @@ class JobOffer extends React.Component {
         </Grid>
         <Grid item xs={2} md={4}>
           {isAdmin
-              ? <Button color="primary" className={classes.button} onClick={() => this.goToJobOfferPage(this.props.company.address, jobOffer.hash)}>
-                Manage
-              </Button>
+              ? <div>
+                {jobOffer.isOpen
+                  ? <Button color="primary" className={classes.button}
+                            onClick={() => this.goToJobOfferPage(this.props.company.address, jobOffer.hash)}>
+                    Manage
+                  </Button>
+                  : null
+                }
+              </div>
               : <div className={classes.button}>
                 <Typography>Status: {jobOffer.isPublished ? '' : 'Not'} Published</Typography>
               </div>
@@ -236,23 +267,65 @@ class JobOffer extends React.Component {
 
           {isAdmin
               ? <div>
-                <Typography variant={"body1"}>Status: {jobOffer.isPublished ? '' : 'Not'} Published</Typography>
+                {jobOffer.isOpen
+                    ? <Typography variant={"body1"}>Status: {jobOffer.isPublished ? '' : 'Not'} Published</Typography>
+                    : <Typography variant={"body1"}>Status: Closed</Typography>
+                }
                 {jobOffer.isPublished && isAdmin
                     ? null
-                    : <Button onClick={() => this.publishJobOffer(jobOffer.hash)} variant={"outlined"} color={"primary"}>Publish</Button>
+                    : <div>
+                      {jobOffer.isOpen
+                          ? <div>
+                            <Button onClick={() => this.publishJobOffer(jobOffer.hash)} variant={"outlined"}
+                                    color={"primary"}>Publish</Button>
+                            <Tooltip
+                                title="Closed job offers will disappear from your page and balance will be made available.">
+                              <Button onClick={() => this.closeJobOffer(jobOffer.hash)} variant={"outlined"}
+                                      color={"primary"}>Close</Button>
+                            </Tooltip>
+                          </div>
+                          : null
+                      }
+                    </div>
                 }
               </div>
               : <div>
-                {userApplied
-                    ? <Typography variant={"body1"}>You have already applied to this job offer</Typography>
+                {jobOffer.isOpen
+                    ? <div>
+                      {userApplied
+                          ? <Typography variant={"body1"}>You have already applied to this job offer</Typography>
+                          : <div>
+                            <input type="file" onChange={this.captureFile}/>
+                            <Button onClick={() => this.applyToJobOffer(jobOffer.hash)} variant={"outlined"}
+                                    color={"default"}>Apply</Button>
+                          </div>
+                      }
+                      </div>
                     : <div>
-                      <input type="file" onChange={this.captureFile}/>
-                      <Button onClick={() => this.applyToJobOffer(jobOffer.hash)} variant={"outlined"}
-                              color={"default"}>Apply</Button>
+                      {this.props.userAddress === jobOffer.approvedApplicant ? <div>
+                            <Typography variant={"body1"}>
+                              Congrats! You were selected! You can claim {jobOffer.rewardInEther} Ether
+                            </Typography>
+                            <Button variant={"outlined"} color={"default"} onClick={() => this.withdraw(jobOffer.rewardInWei)}>
+                              Claim
+                            </Button>
+                          </div>
+                          : <Typography variant={"body1"}>Snap! This job offer is already closed.</Typography>
+                      }
                     </div>
                 }
               </div>
           }
+
+          <Snackbar
+              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              open={this.state.open}
+              onClose={this.handleClose}
+              ContentProps={{
+                'aria-describedby': 'message-id',
+              }}
+              message={<span id="message-id">Amount withdrawn! Check in a couple of seconds your balance!</span>}
+            />
 
           {/* Loading animation on waiting for Ethereum */}
           {this.state.loadingTransaction || this.state.loadingIPFS
